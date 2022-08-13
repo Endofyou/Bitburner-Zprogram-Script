4e67243b30293b6f53f1da268ded5d8d75dc35cf
Endofyou <110877043+Endofyou@users.noreply.github.com>
2022-08-12 21:12:56 -0400

    Update UPDATEDzprogram.js

--- UPDATEDzprogram.js
+++ UPDATEDzprogram.js
@@ -4,7 +4,7 @@ export async function main(ns) {
 
     // ALTER ONE OR BOTH OF THESE CONSTANTS IF NEEDED:
     // These are constants that act as limiters and that may be configured for increased or decreased performance needs:
-    const unitime = 10; // the minimum time allowed between script executions in milliseconds (raise if scripts misalign).
+    const unitime = 10.5; // the minimum time allowed between script executions in milliseconds (raise if scripts misalign).
     const maxscripts = 9000; // the maximum # of scripts that Zprogram is allowed to produce (lower this if game crashes).
 
     const maxinstances = Math.floor(maxscripts / 3);
